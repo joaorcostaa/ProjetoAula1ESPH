ce3be5361ffccc3498e9179e8427ac7b9334d200
Joao Vitor <joao.rcosta@eaportal.org>
2026-03-31 11:44:35 -0300

    feat: estrutura condicional if/if else/switch/ternario

--- src/js/script.js
+++ src/js/script.js
@@ -10,15 +10,55 @@
 /// !=
 
 
+//
 
-const p = 1000;
-const v = 200;
-const precoFinal = (p - v);
-console.log(`O preço final com desconto é de: ${precoFinal}`);
+//function newFunction() {
+//    const p = 1000;
+ //   const v = 200;
+  //  const precoFinal = (p - v);
+ //   console.log(`O preço final com desconto é de: ${precoFinal}`);
+//
+  //  const usuarioLogado = 1;
+    //const token = 100;
+    //const transacaoAutorizada = usuarioLogado === 1 && token >= 10;
+    //console.log("Transaçao autorizada com sucesso", transacaoAutorizada);
 
-const usuarioLogado=1;
-const token = 100;
-const transacaoAutorizada= usuarioLogado === 1 && token >= 10
-console.log("Transaçao autorizada com sucesso", transacaoAutorizada)
+
+    //idade = 18;
+    //if (idade == 18) {
+      //  console.log("Voce é maior de idade");
+    //} else {
+      //  console.log("Voce nao é maior de idade");
+//
+//
+  //  }
+//}
 
 
+let times="santos";
+
+switch(times) {
+    case "santos":
+        console.log("Um time idoso")
+        break;
+    case "sao paulo":
+        console.log("Time Panetone")
+        break;
+    case "palmeiras":
+        console.log("SEM MUNDIAL")
+        break; 
+    case "corinthians":
+        console.log("um time sem idolo")
+        break;
+    default:
+        console.log("tao ruim quanto a seleçao brasileira")      
+} 
+
+let salario=100;
+let resultado = salario ==100? "Salario Certo":"SalarioErrado"
+console.log(resultado)
+
+function verificarParImpar(numero) {
+    return numero % 2 === 0 ? "PAR":"IMPAR"
+}
+console.log(verificarParImpar(4))
