9db143c16aed6ff6d7dfb03afb09eef548692f38
Joao Vitor <joao.rcosta@eaportal.org>
2026-03-31 11:08:32 -0300

    feat: exercicio operadores

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,17 @@
 // ==
 // ===
 /// !=
+
+
+
+const p = 1000;
+const v = 200;
+const precoFinal = (p - v);
+console.log(`O preço final com desconto é de: ${precoFinal}`);
+
+const usuarioLogado=1;
+const token = 100;
+const transacaoAutorizada= usuarioLogado === 1 && token >= 10
+console.log("Transaçao autorizada com sucesso", transacaoAutorizada)
+
+
